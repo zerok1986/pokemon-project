d2c68c3f1c919059d3285435d6c000a37a3b605c
MiguelAngel Abad <miguel.abad86@gmail.com>
2021-09-09 16:20:31 +0200

    create project on git and github

---
+++ app.js
@@ -0,0 +1,28 @@
+let count = 1;
+
+function render() {
+    let container = document.getElementById('pokemon');
+    
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg" >`
+}
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+prev.onclick = function(){
+    if (count > 1) {
+        count--;
+        render();
+    }
+}
+
+next.onclick = function(){
+    if (count < 650) {
+        count++;
+        render();
+    }
+}
+
+
+render();
+
